9496244bfb9549a01b785779cda68fb06941561f
Xiaosong Gao <gxsshallot@163.com>
2023-01-29 03:41:29 +0800

    add badge and make coverage to 100.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # errors
 
+[![status](https://github.com/gaoxiaosong/errors/actions/workflows/go.yml/badge.svg?branch=master)](https://github.com/gaoxiaosong/errors/actions/workflows/go.yml)
 [![codecov](https://codecov.io/gh/gaoxiaosong/errors/branch/master/graph/badge.svg?token=AOXNUDXAS7)](https://codecov.io/gh/gaoxiaosong/errors)
+[![godoc](https://pkg.go.dev/badge/github.com/gaoxiaosong/errors?status.svg)](https://pkg.go.dev/github.com/gaoxiaosong/errors)
+[![apache](https://img.shields.io/badge/License-Apache%202-blue.svg)](https://opensource.org/licenses/Apache-2.0)
--- error_test.go
+++ error_test.go
@@ -11,8 +11,10 @@ func TestNew(t *testing.T) {
 		New(1001, msg),
 		Newf(1002, "%s %s", msg, "args"),
 		NewFull(1003, msg, data, true),
+		NewFull(1003, msg, data, false),
 		NewSubLevel(2001, msg, 2001001, subMsg),
 		NewSubLevelFull(2002, msg, 2001002, subMsg, data, true),
+		NewSubLevelFull(2002, msg, 2001002, subMsg, data, false),
 	}
 	for _, err := range errListInt {
 		if e := testErr[int](err); e != nil {
@@ -23,8 +25,10 @@ func TestNew(t *testing.T) {
 		New("1001", msg),
 		Newf("1002", "%s %s", msg, "args"),
 		NewFull("1003", msg, data, true),
+		NewFull("1003", msg, data, false),
 		NewSubLevel("2001", msg, "2001001", subMsg),
 		NewSubLevelFull("2002", msg, "2001002", subMsg, data, true),
+		NewSubLevelFull("2002", msg, "2001002", subMsg, data, false),
 	}
 	for _, err := range errListStr {
 		if e := testErr[string](err); e != nil {
